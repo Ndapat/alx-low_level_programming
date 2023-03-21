834319ff638a424ae34240649ab2d64a2b704ba1
PATIENCE JOSEPH <patiencejosephndani@gmail.com>
2023-03-21 06:30:57 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints last digit
+ *
+ * @j: function parameter
+ *
+ * Return: k
+ */
+
+int print_last_digit(int j)
+{
+	int k;
+
+	k = j % 10;
+	if (j < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
